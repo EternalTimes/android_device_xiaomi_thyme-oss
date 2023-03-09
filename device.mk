4774220b5cbc11d98ec31e95d5a8fe4d571c2dfa
LynnrinChan <lynnrin@lynnrin.moe>
2023-03-09 11:13:18 +0800

    thyme: Use ih8sn for SN

--- device.mk
+++ device.mk
@@ -297,6 +297,18 @@ PRODUCT_PACKAGES += \
     init.xiaomi.rc \
     ueventd.qcom.rc
 
+# ih8sn
+PRODUCT_PACKAGES += \
+    ih8sn
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ih8sn/ih8sn.conf:/system/etc/ih8sn.conf
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/ih8sn \
+    system/etc/ih8sn.conf \
+    system/etc/init/ih8sn.rc
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
